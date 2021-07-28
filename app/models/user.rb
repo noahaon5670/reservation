d3faae9f5486265f49a4d13a9d6bdc47b325768b
noah3223 <36697791+noah3223@users.noreply.github.com>
2021-07-29 00:54:03 +0900

    fix validate to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,10 @@ class User < ApplicationRecord
   has_many :rooms
   has_many :reserves
   
-  validates :name, presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 30 } ,on: :update
   validates :email, presence: true, length: { maximum: 100 }
   validates :encrypted_password, presence: true
-  validates :icon, presence: true
-  validates :profile, presence: true
+  validates :icon, presence: true,  on: :update
+  validates :profile, presence: true, on: :update
 
 end
