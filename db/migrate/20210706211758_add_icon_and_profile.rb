1e6103cc9c30cb8527a0718731ee8a9add8792f3
noahaon5670 <83285972+noahaon5670@users.noreply.github.com>
2021-07-29 14:19:20 +0900

    Revert "Develop"

--- db/migrate/20210706211758_add_icon_and_profile.rb
+++
@@ -1,6 +0,0 @@
-class AddIconAndProfile < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :icon, :string
-    add_column :users, :profle, :text
-  end
-end
--- db/migrate/20210720173236_remove_room_name_to_reservation.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRoomNameToReservation < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :reserves, :room_name, :string
-  end
-end
