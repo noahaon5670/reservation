eddc3786c63d35cce4ec07de63da5a8aad63b183
noahaon5670 <83285972+noahaon5670@users.noreply.github.com>
2021-07-29 22:54:19 +0900

    Revert "Revert "Revert "Develop"""

--- db/migrate/20210715105950_add_person_num_to_reserves.rb
+++
@@ -1,5 +0,0 @@
-class AddPersonNumToReserves < ActiveRecord::Migration[6.1]
-  def change
-    add_column :reserves, :person_num, :integer
-  end
-end
--- db/migrate/20210720032628_rename_reserve_to_reservation.rb
+++
@@ -1,5 +0,0 @@
-class RenameReserveToReservation < ActiveRecord::Migration[6.1]
-  def change
-    rename_table :reservations, :reserves
-  end
-end
